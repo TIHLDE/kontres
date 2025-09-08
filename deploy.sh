a486c69c1ff51309fe05ca38b1d4b5469fa1fb3d
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-09-08 13:29:35 +0200

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ set -e
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
 echo "-> Building new Docker image"
-docker build --no-cache -t kontres.tihlde.org:$COMMIT_HASH .
+docker build --no-cache -t dev-kontres.tihlde.org:$COMMIT_HASH .
 
 echo "-> Migrating database"
 prisma migrate deploy
 
 echo "-> Stopping and removing old container"
-docker rm -f kontres.tihlde.org || true
+docker rm -f dev-kontres.tihlde.org || true
 
 echo "-> Starting new container"
-docker run --env-file .env -p 9000:3000 --name kontres.tihlde.org --restart unless-stopped -d kontres.tihlde.org:$COMMIT_HASH
+docker run --env-file .env -p 9000:3000 --name dev-kontres.tihlde.org --restart unless-stopped -d dev-kontres.tihlde.org:$COMMIT_HASH
